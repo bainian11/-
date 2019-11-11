5d0812ba0c622e58da133c18735335486b513365
wcm-0425 <271501077@qq.com>
2019-11-11 16:48:01 +0800

    111

---
+++ cool_chat/src/views/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>消息</h3>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Message",
+  components: {
+    
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+  font-size: 34px;
+  font-weight: 400
+}
+</style>
