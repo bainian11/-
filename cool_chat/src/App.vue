6048ca98c9524c54ca4de876968f5b57f533fb9c
shy-0627 <624649666@qq.com>
2019-11-12 19:29:30 +0800

    shy

--- cool_chat/src/App.vue
+++ cool_chat/src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-link to="/myincome"></router-link>
+    <router-link to="/my/myaccount"></router-link>
+    <!-- <router-link :to="'/my/1'"></router-link> -->
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- cool_chat/src/router/index.js
+++ cool_chat/src/router/index.js
@@ -1,28 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/myincome',
+    name: 'myincome',
+    component: () => import('../views/shy/Myincome')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/my/:id',
+    name: 'my',
+    component: () => import('../views/shy/My')
   },
   {
-    path: '/Message',
-    name: 'Message',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Message.vue')
+    path: '/myaccount/:money',
+    name: 'myaccount',
+    component: () => import('../views/shy/Myaccount')
   },
+  {
+    path: '/upphoto',
+    name: 'upphoto',
+    component: () => import('../views/shy/Upphoto'),
+    // children:[]
+  },
+  {
+    path: '/photo',
+    name: 'photo',
+    component: () => import('../views/shy/Photo'),
+
+  }
+
+
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
+  // {
+  //   path: '/Message',
+  //   name: 'Message',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/Message.vue')
+  // },
 ]
 
 const router = new VueRouter({
